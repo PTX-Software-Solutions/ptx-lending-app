bafad57e9702af8ddfe616ac3ef93a459a3b1720
21p <47355155+GeorgeMapili@users.noreply.github.com>
2024-11-03 04:46:32 +0800

    fix: updated the middleware for getting the session (#10)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,9 @@
-import { getSession } from 'next-auth/react'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  const requestForNextAuth = {
-    headers: {
-      cookie: request.headers.get('cookie') ?? undefined
-    }
-  }
-
-  const session = await getSession({ req: requestForNextAuth })
+  const session = !!request.cookies.get('next-auth.session-token')
 
   if (session) {
     return NextResponse.next()
